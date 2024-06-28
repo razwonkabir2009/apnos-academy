3c95cca295132ec59fdca2549631ec1ebc9fd1ed
Razwon Kabir Famim <razwon2009@gmail.com>
2024-06-28 14:18:18 +0600

    Creating the files
    Adding the website's source code.

---
+++ form-action.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<!--Programming By Razwon Kabir Famim. All rights reserved by the developer.-->
+<html lang="en">
+  <head>
+    <meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="APNOS MEDIA 2022 is one of the most interactive and modern media and content creator of Bangladesh. 
+   APNOS TECH is a company owned by them. It provides awesome education to the young about cyber-security and safety.">
+   <meta name="keywords" content="Cyber-security,Malware,Ransomeware,Virus,Privacy,Performane of PC,Bangladeshi site">
+   <meta name="author" content="Razwon Kabir Famim">
+    <link rel="icon" type="image/x-icon" href="images/sitelogo.png"><!--Favicon For the Site.-->
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <style>  @media screen and (max-width: 600px) {
+      .column, input[type=submit] {
+        width: 100%;
+        margin-top: 0;
+      }
+    }</style>
+    <title>APNOS TECH | CONTACT</title>
+  </head>
+<body>
+  <nav>
+    <video autoplay muted loop style="width:100%;max-width:1920px;">
+      <source src="images/site-header.mp4" type="video/mp4">
+    </video>
+  <div class="topnav" id="myTopnav">
+    <a href="index.html" class="madimi-one-regular" style="font-weight: bolder;">APNOS TECH</a>
+    <a href="index.html" class="comfortaa"><i class="fa-solid fa-house"></i>&nbsp;Home</a>
+    <a href="about.html" class="comfortaa"><i class="fa-solid fa-circle-info"></i>&nbsp;About</a>
+    <a href="contact.html" class="active comfortaa"><i class="fa-solid fa-envelope"></i>&nbsp;Contact</a>
+    <a href="Content/Security/security.html" class="comfortaa"><i class="fa-solid fa-shield"></i>&nbsp;Security</a>
+    <a href="Content/Privacy/privacy.html" class="comfortaa"><i class="fa-solid fa-lock"></i>&nbsp;Privacy</a>
+    <a href="Content/Performance/performance.html" class="comfortaa"><i class="fa-solid fa-meteor"></i>&nbsp;Performance</a>
+    <a href="blog.html" class="comfortaa"><i class="fa-solid fa-square-rss"></i>&nbsp;Blog</a>
+    <a class="icon" onclick="toggleResponsiveClass()" name="nav-button"><i class="fa fa-bars"></i></a>
+    <a class="split" onclick="openSearch()" name="search-overlay"><i class="fa fa-search" style="color: white;cursor: pointer;">&nbsp;Search</i></a>
+      <div id="myOverlay" class="overlay">
+        <span class="closebtn" onclick="closeSearch()" title="Close Overlay" style="cursor: pointer;"><i class="fa-solid fa-x"></i></span><br><br><br><br>
+        <div class="overlay-content">
+          <h1 class="merriweather-bold"style="color:white;"><i class="fa-solid fa-magnifying-glass">&nbsp;&nbsp;S e a r c h&nbsp;&nbsp;t h e<br><br>s i t e . . .</i></h1>
+          <form id="searchForm"> 
+            <input type="text" placeholder="Filter Category..." style="border: 5px solid white;border-radius: 25px;" id="mySearch" onkeyup="mySearched()">
+            </form>
+            <ul id="myMenu">
+              <li class="search-head"><a class="comfortaa" href="Content/Security/security.html"><i class="fa-solid fa-shield"></i>&nbsp;Security</a></li>
+              <li class="search-head"><a class="comfortaa" href="Content/Privacy/privacy.html"><i class="fa-solid fa-lock"></i>&nbsp;Privacy</a></li>
+              <li class="search-head"><a class="comfortaa" href="Content/performance/performance.html" ><i class="fa-solid fa-meteor"></i>&nbsp;Performance</a></li>
+              <li class="search-head"><a class="comfortaa" href="Content/Security/Malware/malware.html"><i class="fa-solid fa-bug"></i>&nbsp;&nbsp;Malware</a></li>
+              <li class="search-head"><a class="comfortaa" href="Content/Security/Virus/virus.html"><i class="fa-solid fa-virus"></i>&nbsp;&nbsp;Virus</a></li>
+              <li class="search-head"><a class="comfortaa" href="Content/Security/Ransomware/ransomware.html"><i class="fa-solid fa-circle-dollar-to-slot"></i>&nbsp;&nbsp;Ransomware</a></li>
+              <li class="search-head"><a class="comfortaa" href="Content/Security/Phishing/phishing.html"><i class="fa-solid fa-anchor-lock"></i>&nbsp;&nbsp;Phishing</a></li>
+              <li class="search-head"><a class="comfortaa" href="Content/Security/Scam/scam.html"><i class="fa-solid fa-triangle-exclamation"></i>&nbsp;&nbsp;Scam</a></li>
+              <li class="search-head"><a class="comfortaa" href="Content/Security/Hacking/hacking.html"><i class="fa-solid fa-code"></i>&nbsp;&nbsp;Hacking</a></li>
+              <li class="search-head"><a class="comfortaa" href="Content/Security/Other-threats/other-threats.html"><i class="fa-solid fa-viruses"></i>&nbsp;&nbsp;Other threats</a></li>
+              <li class="search-head"><a class="comfortaa" href="Content/Security/Passwords/passwords.html"><i class="fa-solid fa-key"></i>&nbsp;&nbsp;Passwords</a></li>
+              <li class="search-head"><a class="comfortaa" href="Content/Security/Bussiness/bussiness.html"><i class="fa-solid fa-briefcase"></i>&nbsp;&nbsp;Bussiness</a></li>
+              <li class="search-head"><a class="comfortaa" href="Content/Security/Security-tips/security-tips.html"><i class="fa-solid fa-shield-halved"></i>&nbsp;&nbsp;Security tips</a></li>
+              <li class="search-head"><a class="comfortaa" href="Content/Privacy/Browser/browser.html"><i class="fa-regular fa-window-maximize"></i>&nbsp;&nbsp;Browser</a></li>
+              <li class="search-head"><a class="comfortaa" href="Content/Privacy/IP-Address/IP-Address.html"><i class="fa-solid fa-wifi"></i>&nbsp;&nbsp;IP Address</a></li>
+              <li class="search-head"><a class="comfortaa" href="Content/Privacy/VPN/vpn.html"><i class="fa-solid fa-network-wired"></i>&nbsp;&nbsp;VPN</a></li>
+              <li class="search-head"><a class="comfortaa" href="Content/Privacy/Streaming/streaming.html"><i class="fa-solid fa-video"></i>&nbsp;&nbsp;Streaming</a></li>
+              <li class="search-head"><a class="comfortaa" href="Content/Privacy/Identity-protection/identity-protection.html"><i class="fa-solid fa-user-lock"></i>&nbsp;&nbsp;Identity Protection</a></li>
+              <li class="search-head"><a class="comfortaa" href="Content/Privacy/Dark-web/dark-web.html"><i class="fa-solid fa-globe"></i>&nbsp;&nbsp;Dark Web</a></li>
+              <li class="search-head"><a class="comfortaa" href="Content/Privacy/Digital-life/digital-life.html"><i class="fa-solid fa-fingerprint"></i>&nbsp;&nbsp;Digital life</a></li>
+              <li class="search-head"><a class="comfortaa" href="Content/Privacy/Privacy-tips/privacy-tips.html"><i class="fa-solid fa-lock"></i>&nbsp;&nbsp;Privacy tips</a></li>
+              <li class="search-head"><a class="comfortaa" href="Content/Performance/Speed/speed.html"><i class="fa-solid fa-gauge"></i>&nbsp;&nbsp;Speed</a></li>
+              <li class="search-head"><a class="comfortaa" href="Content/Performance/Cleaning/cleaning.html"><i class="fa-solid fa-broom"></i>&nbsp;&nbsp;Cleaning</a></li>
+              <li class="search-head"><a class="comfortaa" href="Content/Performance/Gaming/gaming.html"><i class="fa-solid fa-gamepad"></i>&nbsp;&nbsp;Gaming</a></li>
+              <li class="search-head"><a class="comfortaa" href="Content/Performance/Hardware/hardware.html"><i class="fa-solid fa-microchip"></i>&nbsp;&nbsp;Hardware</a></li>
+              <li class="search-head"><a class="comfortaa" href="Content/Performance/Software/software.html"><i class="fa-solid fa-window-maximize"></i>&nbsp;&nbsp;Software</a></li>
+              <li class="search-head"><a class="comfortaa" href="Content/Performance/Driver/driver.html"><i class="fa-solid fa-gears"></i>&nbsp;&nbsp;Driver</a></li>
+              <li class="search-head"><a class="comfortaa" href="Content/Performance/Performance-tips/performance-tips.html"><i class="fa-solid fa-bolt"></i>&nbsp;&nbsp;Performance tips</a></li>
+            </ul>
+        </div>
+  </div>
+</nav>
+<span class="content">
+    <header class="header">
+    <h1 class="montserrat-alternates-bold" id="site-header">Contact Us...</h1>
+    <p class="merriweather-regular" id="tagline">Get In Touch...</p></header>
+    <div class="container-con">
+        <?php
+$conn = mysqli_connect("localhost", "id22199110_apnos", "Remote@365", "id22199110_form");
+if($conn === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+$firstname =  $_REQUEST['firstname'];
+$lastname = $_REQUEST['lastname'];
+$email = $_REQUEST['email'];
+$subject = $_REQUEST['subject'];
+$sql = "INSERT INTO `form-data` (firstname, lastname, email, subject) VALUES (?, ?, ?, ?)";
+if($stmt = mysqli_prepare($conn, $sql)){
+    mysqli_stmt_bind_param($stmt, "ssss", $firstname, $lastname, $email, $subject);
+    if(mysqli_stmt_execute($stmt)){
+        echo '<h1 class="comfortaa" id="site-header">Thanks For Contacting us. We will get in touch with you soon.</h1>'; 
+    } else{
+        echo "ERROR: Could not execute query: $sql. " . mysqli_error($conn);
+    }
+} else{
+    echo "ERROR: Could not prepare query: $sql. " . mysqli_error($conn);
+}
+mysqli_stmt_close($stmt);
+mysqli_close($conn);
+?>
+</div>
+<footer class="footer">
+      <span class="foot-link">
+        <img src="images/sitelogo.webp" alt="brand-img" style="width: 200px;height: 200px;margin-left: 5%;">
+        <h1 class="madimi-one-regular brand-name">APNOS TECH</h1>
+      </span>
+      <span class="foot-link">
+        <strong class="baloo-da-2"><i class="fa-solid fa-briefcase"></i>&nbsp;&nbsp;Company</strong><br><br>
+        <a class="baloo-da-2" href="about.html"><i class="fa-solid fa-circle-info"></i>&nbsp;&nbsp;About Us</a><br><br>
+        <a class="baloo-da-2" href="program.html"><i class="fa-solid fa-user"></i>&nbsp;&nbsp;Careers</a><br><br>
+        <a class="baloo-da-2" href="blog.html"><i class="fa-solid fa-square-rss"></i>&nbsp;&nbsp;Blog</a><br><br>
+        <a class="baloo-da-2" href="contact.html"><i class="fa-solid fa-envelope"></i>&nbsp;&nbsp;Get In Touch</a><br><br>
+     </span>
+     <span class="foot-link">
+        <strong class="baloo-da-2"><i class="fa-brands fa-sourcetree"></i>&nbsp;&nbsp;Resources</strong><br><br>
+        <a class="baloo-da-2" href="resources.html"><i class="fa-solid fa-laptop-code"></i>&nbsp;Web-Designing Providers</a><br><br>
+        <a class="baloo-da-2" href="program.html"><i class="fa-solid fa-pen-to-square"></i>&nbsp;Become a Content Creator</a><br><br>
+     </span>
+     <span class="foot-link">
+        <strong class="baloo-da-2"><i class="fa-solid fa-book"></i>&nbsp;&nbsp;Legal</strong><br><br>
+        <a class="baloo-da-2" href="project.html"><i class="fa-solid fa-file-contract"></i>&nbsp;&nbsp;Privacy Policy</a><br><br>
+        <a class="baloo-da-2" href="project.html"><i class="fa-solid fa-cookie-bite"></i>&nbsp;&nbsp;Cookie Policy</a><br><br>
+        <a class="baloo-da-2" href="contact.html"><i class="fa-solid fa-flag"></i>&nbsp;&nbsp;Complaints</a><br><br>
+     </span>
+     <span class="foot-link">
+      <strong class="baloo-da-2"><i class="fa-solid fa-circle-info"></i>&nbsp;&nbsp;About RK GRAPHICS</strong><br><br>
+      <a class="baloo-da-2" href="about.html"><i class="fa-solid fa-code"></i>&nbsp;&nbsp;About the Developer</a><br><br>
+      <a class="baloo-da-2" href="program.html"><i class="fa-solid fa-bullseye"></i>&nbsp;&nbsp;Goals and missions</a><br><br>
+      <a class="baloo-da-2" href="https://razwonkabir2009.rf.gd"><i class="fa-solid fa-square-rss"></i>&nbsp;&nbsp;Personal Blog</a><br><br>
+      <a class="baloo-da-2" href="contact.html"><i class="fa-solid fa-wand-magic-sparkles"></i>&nbsp;&nbsp;Get your own site</a><br><br>
+    </span>
+    <span class="social-link">
+        <h1 class="legal-txt montserrat-alternates-bold">Get Connected with us on...</h1>
+        <a href="https://www.facebook.com/APNOS22" aria-label="Facebook Page"><i class="fa-brands fa-facebook"></i></a>
+        <a href="https:/www.instagram.com/apnosmedia2022" aria-label="Instagram Profile"><i class="fa-brands fa-instagram"></i></a>
+        <a href="https://discord.gg/yxw4EqV2pc" aria-label="Discord Server"><i class="fa-brands fa-discord"></i></a>
+        <a href="tel:+8809638231796" aria-label="Phone Number"><i class="fa-solid fa-phone"></i></a>
+        <a href="mailto:academy@apnosmedia2022.rf.gd?subject=Contact&body=I want to contact you." aria-label="Email"><i class="fa-solid fa-envelope"></i></a>
+    </span>
+    <span class="foot-legal">
+      <div class="legal-box">
+      <strong><h1 class="legal-txt comfortaa">© 2024. ALL RIGHTS RESERVED BY THE TEAM. A PART OF</h1></strong>
+      <p class="legal-txt lobster-two-bold" style="text-align: center;">APNOS MEDIA 2022.</p>
+    </div>
+    </span>
+    <span class="foot-dev-name">
+      <div class="dev-box">
+    <h1 class="legal-txt source-code-pro-bold">DESIGN AND WEB DEVELOPMENT BY</h1>
+      <p class="legal-txt knewave-regular" style="text-align: center;">RAZWON KABIR FAMIM</p>
+    </div>
+    </span>
+     </footer>
+  </span>
+  <script async charset="UTF-8" src="javascripts/script.js" type="text/javascript"></script>
+  <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+  (function(){
+  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+  s1.async=true;
+  s1.src='https://embed.tawk.to/660e7f4f1ec1082f04ded560/1hqka2k54';
+  s1.charset='UTF-8';
+  s1.setAttribute('crossorigin','*');
+  s0.parentNode.insertBefore(s1,s0);
+  })();
+  </script>
+  <!--End of Tawk.to Script-->
+  <!-- Google tag (gtag.js) -->
+<script async charset="UTF-8" src="https://www.googletagmanager.com/gtag/js?id=G-K0XBFPHH49"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-K0XBFPHH49');
+</script>
+<!-- Google tag (gtag.js) -->
+<script async charset="UTF-8" src="https://kit.fontawesome.com/9419d875f5.js" crossorigin="anonymous"></script>
+</body>
+</html>
